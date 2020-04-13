f80f7f078f2566719411eea47ef9f942f93394f0
Fahad <fahadsheikh3010@users.noreply.github.com>
2020-04-13 10:14:45 +0530

    Navigation

---
+++ Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            print("hi");
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+                print("hello");
+            }
+        }
+    }
+}
