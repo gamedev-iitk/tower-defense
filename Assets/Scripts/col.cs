064310d2f5aa1200be81ab308181d20d9eeede8a
Restyled.io <commits@restyled.io>
2020-04-14 16:38:49 +0000

    Restyled by clang-format

--- Assets/Scripts/col.cs
+++ Assets/Scripts/col.cs
@@ -2,51 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class col : MonoBehaviour
-{
-    Renderer rend;
-    int colorPicker = 4;
-    bool trigg = false;
-    void Start()
-    {
-        rend = GetComponent<Renderer>();
+public class col : MonoBehaviour {
+  Renderer rend;
+  int colorPicker = 4;
+  bool trigg = false;
+  void Start() { rend = GetComponent<Renderer>(); }
+  // Start is called before the first frame update
+  void OnTriggerEnter(Collider other) {
 
+    trigg = true;
+    colorPicker = 1;
+  }
+
+  void OnTriggerExit(Collider other) {
+    trigg = false;
+    colorPicker = 0;
+  }
+
+  void Update() {
+    // rend.material.color.a = 0.3f;
+
+    switch (colorPicker) {
+    case 0:
+      rend.material.color = Color.green;
+      break;
+    case 1:
+      rend.material.color = Color.red;
+      break;
+
+    default:
+      rend.material.color = Color.white;
+      break;
     }
-    // Start is called before the first frame update
-    void OnTriggerEnter(Collider other)
-    {
-
-        trigg = true;
-        colorPicker = 1;
-
-
-
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        trigg = false;
-        colorPicker = 0;
-
-    }
-
-    void Update()
-    {
-        // rend.material.color.a = 0.3f;
-
-        switch (colorPicker)
-        {
-        case 0:
-            rend.material.color = Color.green;
-            break;
-        case 1:
-            rend.material.color = Color.red;
-            break;
-
-        default:
-            rend.material.color = Color.white;
-            break;
-        }
-
-    }
+  }
 }
--- Assets/Scripts/dragmesh.cs
+++ Assets/Scripts/dragmesh.cs
@@ -2,51 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dragmesh : MonoBehaviour
-{
+public class dragmesh : MonoBehaviour {
 
-    bool trigg = false;
-    private float mZCoord;
-    private Vector3 mOffset;
-    public GameObject plane;
-    Renderer rend;
-    int colorPicker = 4;
-    void Start()
-    {
-        rend = GetComponent<Renderer>();
+  bool trigg = false;
+  private float mZCoord;
+  private Vector3 mOffset;
+  public GameObject plane;
+  Renderer rend;
+  int colorPicker = 4;
+  void Start() { rend = GetComponent<Renderer>(); }
 
-    }
+  void OnMouseDown() {
 
-    void OnMouseDown()
-    {
+    mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+    mOffset = gameObject.transform.position - GetMouseWorldPos();
+  }
 
-        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        mOffset = gameObject.transform.position - GetMouseWorldPos();
+  private Vector3 GetMouseWorldPos() {
+    Vector3 mousePoint = Input.mousePosition;
+    mousePoint.z = mZCoord;
+    return Camera.main.ScreenToWorldPoint(mousePoint);
+  }
+  void OnMouseDrag() {
+    Vector3 coord = GetMouseWorldPos();
+    Vector3 coord1 = new Vector3(coord.x, 5.28f, coord.z) +
+                     new Vector3(mOffset.x, 2f, mOffset.z);
+    Vector3 coord2 = new Vector3(coord.x, 0.01f, coord.z) +
+                     new Vector3(mOffset.x, 0, mOffset.z);
+    transform.position = coord1;
+    plane.transform.position = coord2;
 
-
-    }
-
-    private Vector3 GetMouseWorldPos()
-    {
-        Vector3 mousePoint = Input.mousePosition;
-        mousePoint.z = mZCoord;
-        return Camera.main.ScreenToWorldPoint(mousePoint);
-    }
-    void OnMouseDrag()
-    {
-        Vector3 coord = GetMouseWorldPos();
-        Vector3 coord1 = new Vector3(coord.x, 5.28f, coord.z) + new Vector3(mOffset.x, 2f, mOffset.z);
-        Vector3 coord2 = new Vector3(coord.x, 0.01f, coord.z) + new Vector3(mOffset.x, 0, mOffset.z);
-        transform.position = coord1;
-        plane.transform.position = coord2;
-
-        if (trigg) {
-            colorPicker = 1;
-        }
-        else colorPicker = 0;
-
-
-    }
-
-
+    if (trigg) {
+      colorPicker = 1;
+    } else
+      colorPicker = 0;
+  }
 }
