7b568f74f6c2b1e75bbb3813a028f00709fd5ee4
Mayant Mukul <mayantmukul@hotmail.com>
2020-04-16 19:58:01 +0530

    More documentation

--- Assets/Scripts/Events/TDEvents.cs
+++ Assets/Scripts/Events/TDEvents.cs
@@ -1,11 +1,19 @@
-using System;
 using UnityEngine.Events;
 
-[Serializable]
+/// <summary>
+/// An event that takes callbacks with no parameters
+/// </summary>
 public class TDEvent : UnityEvent { }
 
-[Serializable]
+/// <summary>
+/// An event that takes callbacks with one parameter
+/// </summary>
+/// <typeparam name="TParam">Type of the parameter</typeparam>
 public class TDEvent<TParam> : UnityEvent<TParam> { }
 
-[Serializable]
+/// <summary>
+/// An event that takes callbacks with two parameters
+/// </summary>
+/// <typeparam name="TParam1">Type of the first parameter</typeparam>
+/// <typeparam name="TParam2">Type of the second parameter</typeparam>
 public class TDEvent<TParam1, TParam2> : UnityEvent<TParam1, TParam2> { }
