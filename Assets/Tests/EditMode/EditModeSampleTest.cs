bdf4f388a523cc667a3302e8b141767c90b60e4a
Mayant Mukul <mayantmukul@hotmail.com>
2020-04-20 00:04:48 +0530

    Restructure tests

---
+++ Assets/Tests/EditMode/EditModeSampleTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class EditModeSampleTest
+{
+    [Test]
+    public void EditModeSampleTestSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    [UnityTest]
+    public IEnumerator EditModeSampleTestWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
---
+++ Assets/Tests/PlayMode/MovementTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+public class MovementTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    [TearDown]
+    public void Teardown() { }
+
+    [UnityTest]
+    public IEnumerator ControllerMovesPlayer()
+    {
+        GameObject player = GameObject.Find("Player");
+        Vector3 initial = player.transform.position;
+        player.GetComponent<AIController>().MoveTo(initial + new Vector3(-1, 0, -1));
+
+        yield return new WaitForSeconds(2f);
+        Assert.AreNotEqual(initial, player.transform.position);
+    }
+}
+
