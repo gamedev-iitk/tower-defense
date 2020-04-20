21ff977e264081df776efbbbf68661875e326203
Mayant Mukul <mayantmukul@hotmail.com>
2020-04-20 21:52:12 +0530

    Add damageable component

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    private float health = 100f;
+
+    /// <summary>
+    /// Subtracts the given value from health and triggers death if health reaches 0
+    /// </summary>
+    /// <param name="damage">Value to be subtracted</param>
+    public void ApplyDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            StartDeath();
+        }
+    }
+
+    private void StartDeath()
+    {
+        // 
+    }
+}
