591c23d3c98f16a9088c08797dccbb2447749152
sps1112 <62471966+sps1112@users.noreply.github.com>
2020-04-21 18:31:00 +0530

    merge tower attack

---
+++ Assets/Scripts/Other/Battle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battle : MonoBehaviour
+{
+  public bool isFighting=false;
+  public float attack,range,rateOfFire;
+
+  public bool isEnemyDead=true;
+  public float timer;
+
+  public GameObject enemytoTarget;
+
+   void Start()
+   {
+        GetComponent<CapsuleCollider>().radius=range;
+   }
+  public void Attack(GameObject enemy)
+  {
+      Debug.Log("Started fighting");
+      enemytoTarget=enemy;
+      if(enemytoTarget.GetComponent<isDamageable>()!=null)
+      {
+          Debug.Log("enemy can be damaged");
+          isEnemyDead=false;
+      }
+      isFighting=true;
+      timer=0f;
+  }
+
+  public void stopAttack()
+  {
+      Debug.Log("stopped fighting");
+      isFighting=false;
+      enemytoTarget=null;
+  }
+
+  void Update()
+  {
+      timer+=Time.deltaTime;
+      if(timer>=rateOfFire)
+      {
+         if(isFighting)
+        {
+          Debug.Log("Attack");
+          if(!isEnemyDead)
+          {
+              isEnemyDead=enemytoTarget.GetComponent<isDamageable>().Damage(attack);
+              Debug.Log("damage Done");
+              if(isEnemyDead)
+              {
+                Debug.Log("enemy is dead");
+                GameObject.Destroy(enemytoTarget);
+                stopAttack();
+                this.GetComponent<Detection>().shouldRotate=false;
+                Debug.Log("attack has been stopped");
+              }
+          }  
+        }
+        timer=0f;
+      }
+  }
+
+}
---
+++ Assets/Scripts/Other/Detection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour
+{
+    public Battle battle;
+  public GameObject enemy;
+
+  public GameObject target;
+  public bool shouldRotate=false;
+
+  void OnTriggerEnter(Collider other)
+  {
+      Debug.Log("entered");
+      if(other.gameObject.tag=="Enemy")
+      {
+          Debug.Log("Called for attack");
+          target=other.gameObject;
+          shouldRotate=true;
+          battle.Attack(target);
+      }
+  }
+
+  void OnTriggerExit(Collider other)
+  {
+      Debug.Log("exited");
+      shouldRotate=false;
+      battle.stopAttack();
+  }
+
+  void Update()
+  {
+      if(shouldRotate)
+      {
+          Vector3 look=transform.position-target.transform.position;
+          look.y=0;
+          Quaternion targetRotation=Quaternion.LookRotation(look);
+          transform.rotation=Quaternion.Lerp(transform.rotation,targetRotation,0.1f);
+      }
+  }
+}
