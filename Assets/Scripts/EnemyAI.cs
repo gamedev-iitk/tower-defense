2b1f2e65e4506d0ab7970a13592ba2fd683c45e5
Fahad Sheikh <fahadsheikh3010@gmail.com>
2020-04-27 19:43:09 +0530

    Enemy AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    //private GameObject enemy;
+    public Transform[] waypoints;
+    private int m_Index=0;
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent =GetComponent<NavMeshAgent>();
+        agent.SetDestination(waypoints[0].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance < agent.stoppingDistance)
+        {
+            m_Index = m_Index+1 > waypoints.Length-1 ? waypoints.Length-1 : m_Index+1;
+            agent.SetDestination(waypoints[m_Index].position);
+        }
+    }
+}
