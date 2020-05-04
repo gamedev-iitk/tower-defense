000680988a173b26007101cd5a7646a7ca3a9b05
rahulv73039 <arahulv@iitk.ac.in>
2020-05-04 16:03:24 +0530

    main menu and pause menu addedd

---
+++ Assets/Scripts/mmenuui.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class mmenuui : MonoBehaviour
+{
+   
+    public void PlayButton ()
+    {
+        Debug.Log("Intialising Game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        Time.timeScale = 1f;
+    } 
+    public void QuitButton()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+
+    } 
+    public void ReturnTomain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1 );
+    } 
+     
+    public void pauseButton()
+    {
+        Time.timeScale = 0f;
+    } 
+    public void continueButton()
+    {
+        Time.timeScale = 1f;
+    } 
+  
+}
---
+++ Assets/Scripts/settingUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class settingUI : MonoBehaviour
+
+{
+    string pname;
+    public Light sunLight;
+    GameObject sliderInitialValue;
+    void start()
+    {
+        sunLight = GetComponent<Light>();
+      
+
+    }
+   public void setBrightness(float sliderValue)
+    {
+        
+        sunLight.intensity = sliderValue * 2;
+    } 
+    public void getName(string name)
+    {
+        pname = name;
+    }
+}
