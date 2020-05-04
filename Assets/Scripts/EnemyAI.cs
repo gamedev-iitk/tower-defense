cfbd264dafec9a81dbb52be9da42a52f3e967b74
Fahad Sheikh <fahadsheikh3010@gmail.com>
2020-05-04 13:23:57 +0530

    Enemy AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,25 +1,76 @@
-﻿using UnityEngine;
+﻿using System.Net.Sockets;
+using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Playables;
 
 public class EnemyAI : MonoBehaviour
 {
     //private GameObject enemy;
-    public Transform[] waypoints;
-    private int m_Index=0;
+    public Transform start,end;
+    public bool smart;
+    private Transform enemy;
     private NavMeshAgent agent;
+    public bool Move;
     void Start()
     {
-        agent =GetComponent<NavMeshAgent>();
-        agent.SetDestination(waypoints[0].position);
+        agent = GetComponent<NavMeshAgent>();
+        enemy = GetComponent<Transform>();
+        agent.SetDestination(start.position);
+        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Smart_Move(NavMeshAgent m_agent)
     {
-        if (agent.remainingDistance < agent.stoppingDistance)
+       // m_Index = m_Index + 1 > waypoints.Length - 1 ? waypoints.Length - 1 : m_Index + 1;
+        m_agent.SetDestination(end.position);
+        Attack(m_agent);
+    }
+
+    void FixedUpdate()
+    {
+        if (Move)
         {
-            m_Index = m_Index+1 > waypoints.Length-1 ? waypoints.Length-1 : m_Index+1;
-            agent.SetDestination(waypoints[m_Index].position);
+            ToMove(smart, agent);
+            Vector3 dir = end.position - enemy.position;
+            Debug.DrawRay(enemy.position, dir);
+        }
+    }
+    
+    void ToMove(bool m_smart, NavMeshAgent m_agent)
+    {
+        if (m_smart)
+        {
+             Smart_Move(m_agent);
+        }
+        else
+        {
+            Dumb_Move(m_agent);
+        }
+    }
+
+    void Dumb_Move(NavMeshAgent m_agent)
+    {
+        Vector3 dir = end.position - enemy.position;
+        Ray vision_ray = new Ray(enemy.position, dir);
+        int mask = LayerMask.GetMask("Enemy");
+
+        if (Physics.Raycast(vision_ray, out RaycastHit hit, mask))
+        {
+            Transform target = hit.transform;
+            if (!target.CompareTag("Enemy"))
+            {
+                m_agent.SetDestination(target.position);
+                Attack(m_agent);
+            }
+        }
+    }
+    void Attack(NavMeshAgent m_agent)
+    {
+        if(m_agent.remainingDistance <= m_agent.stoppingDistance)
+        {
+            Debug.Log("attack");
         }
     }
 }
+
