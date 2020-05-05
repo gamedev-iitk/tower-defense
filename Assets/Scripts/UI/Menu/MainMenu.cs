319fb8910cdb4f820005182e9ea6cc202c6d67cc
Mayant Mukul <mayantmukul@hotmail.com>
2020-05-05 18:51:35 +0530

    Refactor the MainMenu

---
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private DialogSystem dialog;
+
+    // TODO: Use async operations and a loading screen to change scenes
+
+    private void Start()
+    {
+        dialog = transform.Find("Dialog").GetComponent<DialogSystem>();
+    }
+
+    public void OnPlay()
+    {
+        Debug.Log("Pressed Play. Loading MainScene.");
+        SceneManager.LoadScene("MainScene");
+        Time.timeScale = 1f;
+
+        SceneManager.UnloadSceneAsync("MainMenu");
+    }
+
+    public void OnHelp()
+    {
+        // Show help
+    }
+
+    public void OnSettings()
+    {
+        // Show Settings
+    }
+
+    public void OnQuit()
+    {
+        DialogConfig config = new DialogConfig
+        {
+            Message = "Are you sure you want to quit?",
+            OK = new DialogButton(
+                text: "Yes",
+                onClick: Application.Quit
+            ),
+            Cancel = new DialogButton(
+                text: "Cancel"
+            )
+        };
+
+        dialog.Show(config);
+    }
+
+    public void ReturnTomain()
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+    public void pauseButton()
+    {
+        Time.timeScale = 0f;
+    }
+    public void continueButton()
+    {
+        Time.timeScale = 1f;
+    }
+}
