9ca57e9676ad577844024f888c512ce790914be5
Fahad Sheikh <fahadsheikh3010@gmail.com>
2020-05-09 16:25:43 +0530

    Mine Tower

---
+++ Assets/Scripts/Towers/MineTower.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineTower : AbstractBattle
+{
+    //private bool isFighting = false;
+    private float timer;
+
+
+    override protected void SetRange()
+    {
+        GetComponent<SphereCollider>().radius = Range;
+    }
+    
+    override public void OnDetect(GameObject enemy)
+    {
+        // Starts conditions for an attack
+        //target = enemy;
+        //enemyDamageable = target.GetComponent<Damageable>();
+        //isFighting = true;
+        timer = 0f;
+    }
+
+    override public void OnLose()
+    {
+        //isFighting = false;
+        //target = null;
+        ////GetComponent<RangeDetection>().Reset();
+    }
+
+    void Update()
+    {
+        // Checks if to attack an enemy      {
+            // Start the timer for FireRate
+            timer += Time.deltaTime;
+            if (timer >= FireRate)
+            {
+                GetComponent<RangeDetection>().DetectEnemys();
+                //if (enemyDamageable.IsDead)
+                //{
+                //    OnLose();
+                //}
+
+                // TODO: Decide if we should do raycasts first, when most tower attacks are going to be area-of-effect
+                // Vector3 direction = target.transform.position - transform.position;
+                // if (Physics.Raycast(transform.position, direction, out RaycastHit hit)) {}
+
+                // Reset the timer
+                timer = 0f;
+        }
+    }
+}
+
