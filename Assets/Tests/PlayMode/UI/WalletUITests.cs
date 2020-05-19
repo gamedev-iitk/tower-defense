cba7a0ef3b33210c817b1a99c0d090e5e44456d2
sps1112 <62471966+sps1112@users.noreply.github.com>
2020-05-19 19:57:38 +0530

    add wallet and dialog tests

--- Assets/Tests/PlayMode/UI/WalletUITests.cs
+++ Assets/Tests/PlayMode/UI/WalletUITests.cs
@@ -3,6 +3,7 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Tests.UI
 {
@@ -18,9 +19,35 @@ namespace Tests.UI
         }
 
         [UnityTest]
-        public IEnumerator CanCreateUpgradeUIMenu()
+        public IEnumerator DisplaysCurrentCash()
         {
+            GameObject walletUI = GameObject.Find("UIManager").transform.Find("WalletUI").gameObject;
+            GameObject cashText = walletUI.transform.Find("WalletBG/Cash").gameObject;
             yield return null;
+            Assert.AreEqual("$ " + GameState.CurrentCash, cashText.GetComponent<Text>().text);
+        }
+
+        [UnityTest]
+        public IEnumerator CashUICanChange()
+        {
+            GameObject walletUI = GameObject.Find("UIManager").transform.Find("WalletUI").gameObject;
+            GameObject cashText = walletUI.transform.Find("WalletBG/Cash").gameObject;
+            string originalText = cashText.GetComponent<Text>().text;
+            GameState.CurrentCash -= 1;
+            yield return null;
+            Assert.AreNotEqual(originalText, cashText.GetComponent<Text>().text);
+        }
+
+        [UnityTest]
+        public IEnumerator CashDeductedUponAction()
+        {
+            GameObject walletUI = GameObject.Find("UIManager").transform.Find("WalletUI").gameObject;
+            int originalCash = GameState.CurrentCash;
+            TDEvent<ETowerType> showMoveDialog = EventRegistry.GetEvent<ETowerType>("showMoveDialog");
+            showMoveDialog.Invoke(ETowerType.Base);
+            walletUI.GetComponent<WalletUISystem>().OnOKClickMove();
+            yield return null;
+            Assert.AreNotEqual(originalCash, GameState.CurrentCash);
         }
     }
 }
