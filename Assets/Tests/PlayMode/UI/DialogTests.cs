e5ec7ff19326e9d3d5fb8b7c33cc9fe5d0af8a30
Siddhartha Pratap Singh <62471966+sps1112@users.noreply.github.com>
2020-05-22 09:47:17 +0530

    Add WalletUI and Dialog tests

--- Assets/Tests/PlayMode/UI/DialogTests.cs
+++ Assets/Tests/PlayMode/UI/DialogTests.cs
@@ -3,6 +3,7 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Tests.UI
 {
@@ -18,9 +19,58 @@ namespace Tests.UI
         }
 
         [UnityTest]
-        public IEnumerator CanCreateUpgradeUIMenu()
+        public IEnumerator UIShowsUpOnMoveClick()
         {
+            TDEvent<ETowerType> showMoveDialog = EventRegistry.GetEvent<ETowerType>("showMoveDialog");
+            showMoveDialog.Invoke(ETowerType.Base);
+            GameObject dialogBox = GameObject.Find("UIManager").transform.Find("WalletUI/Dialog").gameObject;
             yield return null;
+            Assert.True(dialogBox.activeSelf);
+        }
+
+        [UnityTest]
+        public IEnumerator UIShowsUpOnUpgradeClick()
+        {
+            TDEvent<ETowerType> showUpgradeDialog = EventRegistry.GetEvent<ETowerType>("showUpgradeDialog");
+            showUpgradeDialog.Invoke(ETowerType.Base);
+            GameObject dialogBox = GameObject.Find("UIManager").transform.Find("WalletUI/Dialog").gameObject;
+            yield return null;
+            Assert.True(dialogBox.activeSelf);
+        }
+
+        [UnityTest]
+        public IEnumerator UIGoesAwayOnOKClick()
+        {
+            TDEvent<ETowerType> showMoveDialog = EventRegistry.GetEvent<ETowerType>("showMoveDialog");
+            showMoveDialog.Invoke(ETowerType.Base);
+            GameObject dialogBox = GameObject.Find("UIManager").transform.Find("WalletUI/Dialog").gameObject;
+            yield return null;
+            dialogBox.GetComponent<DialogSystem>().OKClicked();
+            yield return null;
+            Assert.False(dialogBox.activeSelf);
+        }
+
+        [UnityTest]
+        public IEnumerator UIGoesAwayOnCancelClick()
+        {
+            TDEvent<ETowerType> showMoveDialog = EventRegistry.GetEvent<ETowerType>("showMoveDialog");
+            showMoveDialog.Invoke(ETowerType.Base);
+            GameObject dialogBox = GameObject.Find("UIManager").transform.Find("WalletUI/Dialog").gameObject;
+            yield return null;
+            dialogBox.GetComponent<DialogSystem>().CancelClicked();
+            yield return null;
+            Assert.False(dialogBox.activeSelf);
+        }
+
+        [UnityTest]
+        public IEnumerator UIButtonTextDependsOnConfig()
+        {
+            TDEvent<ETowerType> showMoveDialog = EventRegistry.GetEvent<ETowerType>("showMoveDialog");
+            showMoveDialog.Invoke(ETowerType.Base);
+            GameObject dialogBox = GameObject.Find("UIManager").transform.Find("WalletUI/Dialog").gameObject;
+            GameObject okButtonText = dialogBox.transform.Find("OKButton/Text").gameObject;
+            yield return null;
+            Assert.AreEqual("Move", okButtonText.GetComponent<Text>().text);
         }
     }
 }
